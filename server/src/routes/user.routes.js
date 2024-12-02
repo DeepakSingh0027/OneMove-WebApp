c399ef8caa1aa916828a5ae92681f21c868e4989
DeepakSingh0027 <149071400+DeepakSingh0027@users.noreply.github.com>
2024-12-02 19:03:00 +0530

    bug

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,60 +1,35 @@
-import {   Router } from "express"
-import { changeCurrentPassword, 
-    getCurrentUser, 
-    loginUser,
-    logoutUser,
-    refreshAccessToken,
-    registerUser, 
-    updateAccountDetails,
-    updateRole
-    } from "../controllers/user.controller.js"
-import { getUserProducts } from "../controllers/product.controller.js"
-import { upload } from "../middlewares/multer.middleware.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { Router } from "express";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateRole,
+} from "../controllers/user.controller.js";
+import { getUserProducts } from "../controllers/product.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
+const router = Router();
+router.route("/register").post(registerUser);
 
-const router = Router()
-router.route("/register").post(
-    registerUser
-)
- 
-router.route("/get-user-products").get(
-    verifyJWT,
-    getUserProducts
-)
+router.route("/get-user-products").get(verifyJWT, getUserProducts);
 
-router.route("/login").post(
-    loginUser
-)
+router.route("/login").post(loginUser);
 
-router.route("/logout").post(
-    verifyJWT,
-    logoutUser
-)
+router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route("/refresh-access-token").post(
-    refreshAccessToken
-)
+router.route("/refresh-access-token").post(refreshAccessToken);
 
-router.route("/change-password").patch(
-    verifyJWT,
-    changeCurrentPassword
-)
+router.route("/change-password").patch(verifyJWT, changeCurrentPassword);
 
-router.route("/current-user").get(
-    verifyJWT,
-    getCurrentUser
-)
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 
-router.route("/update-details").patch(
-    verifyJWT,
-    updateAccountDetails
-)
+router.route("/update-details").patch(verifyJWT, updateAccountDetails);
 
-router.route("/update-role").patch(
-    verifyJWT,
-    updateRole
-)
+router.route("/update-role").patch(verifyJWT, updateRole);
 
-
-export default router
+export default router;
