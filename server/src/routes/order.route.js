824971151a1dcbd7df25fa5b79eacd286ca1991d
DeepakSingh0027 <149071400+DeepakSingh0027@users.noreply.github.com>
2024-12-02 19:08:28 +0530

    bug

--- server/src/routes/order.route.js
+++ server/src/routes/order.route.js
@@ -1,26 +1,18 @@
-import {Router} from "express"
-import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-import { getOrder, 
-    getOrder, 
-    getOrderSold, 
-    listOrder} from "../controllers/order.controller.js"
+import {
+  getOrder,
+  getOrderSold,
+  listOrder,
+} from "../controllers/order.controller.js";
 
-const router = Router()
+const router = Router();
 //both
-router.route("/get-order").get(
-    verifyJWT,
-    getOrder
-)
+router.route("/get-order").get(verifyJWT, getOrder);
 //seller
-router.route("/get-order-sold").get(
-    verifyJWT,
-    getOrderSold
-)
+router.route("/get-order-sold").get(verifyJWT, getOrderSold);
 //buyer
-router.route("/list-order").post(
-    verifyJWT,
-    listOrder
-)
+router.route("/list-order").post(verifyJWT, listOrder);
 
-export default router
+export default router;
